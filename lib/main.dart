b0505c5cd9afe224fac42ba7d0799097ab701bdc
NAMAN <namanaggarwalg@gmail.com>
2020-10-02 11:19:16 +0530

    Added dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Color(0xFF3EBACE),
         accentColor: Color(0xFFD8ECF1),
         scaffoldBackgroundColor: Color(0xFFF3F5F7),
+        secondaryHeaderColor: Colors.grey
       ),
       darkTheme: ThemeData(
         scaffoldBackgroundColor: Colors.blueGrey[700],
